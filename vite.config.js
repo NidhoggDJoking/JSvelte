c300be06ad471b0a0d151d0b7597633f67a32c5a
NidhoggDJoking <38490462+NidhoggDJoking@users.noreply.github.com>
2022-12-15 11:34:00 +0800

    1 file modified

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    // Vite路径别名配置
+    alias: {
+      '@': path.resolve('./src')
+    }
+  }
 })
