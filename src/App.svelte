58d2f9710d30b044aa9c96791b2fc1493bc335f4
NidhoggDJoking <38490462+NidhoggDJoking@users.noreply.github.com>
2022-12-19 11:49:20 +0800

    3 files modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Router, Link, Route } from "svelte-routing";
-  import { navlink } from '@/styles/index';
+  import { navlink, navPosition } from '@/styles/index';
   import Index from '@/lib/Index.svelte'
   import Animations from '@/lib/Animations.svelte'
   import Transition from '@/lib/Transition.svelte'
@@ -10,21 +10,33 @@
 
 <main>
   <Router url="{url}">
-  <nav>
-    <Link to="/">index</Link>
-    <Link to="transition">transition</Link>
-    <Link to="animations">animations</Link>
-  </nav>
-  <div>
-    <Route path="/">
-      <Index />
-    </Route>
-    <Route path="transition">
-      <Transition />
-    </Route>
-    <Route path="animations">
-      <Animations/>
-    </Route>
-  </div>
-</Router>
+    <nav class={navPosition}>
+      <Link to="/">index</Link>
+      <Link to="transition">transition</Link>
+      <Link to="animations">animations</Link>
+    </nav>
+    <div>
+      <Route path="/">
+        <Index />
+      </Route>
+      <Route path="transition">
+        <Transition />
+      </Route>
+      <Route path="animations">
+        <Animations/>
+      </Route>
+    </div>
+  </Router>
 </main>
+
+
+<style>
+main{
+  height: 100%;
+  width: 100%;
+  position: relative;
+  padding: 0;
+  margin: 0;
+  border: 0;
+}
+</style>
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,4 @@
-import { css } from '@emotion/css'
-
+import { css } from '@emotion/css';
 
 const brand = '#74D900';
 
@@ -28,4 +27,11 @@ export const navlink = css`
   background-clip: text;
   -webkit-background-clip: text;
   background: linear-gradient(to right, #00bcff, #00ff96,#fbff00,#ff7600);
+`;
+
+export const navPosition = css`
+  position: absolute;
+  width: 100%;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 10%);
+  top: 0px;
 `;
