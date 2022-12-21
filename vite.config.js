99593d799587e8092aae7e26f2f955f5302cbb7f
NidhoggDJoking <38490462+NidhoggDJoking@users.noreply.github.com>
2022-12-21 10:28:57 +0800

    2 files modified, 1 added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(),
+    '@babel/plugin-proposal-optional-chaining',// 可选链
+    '@babel/plugin-proposal-nullish-coalescing-operator'//双问号
+  ],
   resolve: {
     // Vite路径别名配置
     alias: {
