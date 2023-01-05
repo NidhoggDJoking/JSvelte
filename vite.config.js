17f05e5ed5f236078ef9371134cb8fd3dfc496b8
NidhoggDJoking <38490462+NidhoggDJoking@users.noreply.github.com>
2023-01-05 10:04:41 +0800

    2 files modified, 1 added

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     '@babel/plugin-proposal-optional-chaining',// 可选链
     '@babel/plugin-proposal-nullish-coalescing-operator'//双问号
   ],
+  base: '/JSvelte/',
   envPrefix: 'VITE_',// 以 envPrefix 开头的环境变量会通过 import.meta.env 暴露在你的客户端源码中。默认： VITE_
   envDir: 'root', // 用于加载 .env 文件的目录。可以是一个绝对路径，也可以是相对于项目根的路径。
   resolve: {
@@ -23,6 +24,14 @@ export default defineConfig({
     cssCodeSplit: true, // 启用/禁用 CSS 代码拆分。当启用时，在异步 chunk 中导入的 CSS 将内联到异步 chunk 本身，并在其被加载时插入。如果禁用，整个项目中的所有 CSS 将被提取到一个 CSS 文件中。
     sourcemap: true, // 如果为 true，将会创建一个独立的 source map 文件
     reportCompressedSize: false, // 启用/禁用 gzip 压缩大小报告。压缩大型输出文件可能会很慢，因此禁用该功能可能会提高大型项目的构建性能。
+    manifest: false,
+    rollupOptions: {
+      output: {
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+      }
+    }
   },
   server:{
     port: 9527, // 指定开发服务器端口。注意：如果端口已经被使用，Vite 会自动尝试下一个可用的端口，所以这可能不是开发服务器最终监听的实际端口。
